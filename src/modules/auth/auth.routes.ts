00204adb6f40bf607a5b7f43f24f32b8f24e859d
altf4rex <mr.stereo2002@gmail.com>
2025-02-15 20:35:27 +0300

    implemented authentication module and midd;eware for verifying

---
+++ src/modules/auth/auth.routes.ts
@@ -0,0 +1,21 @@
+import { Router } from 'express';
+import { check } from 'express-validator';
+import * as AuthController from './auth.controller';
+import { validate } from '../../middlewares/validation.middleware';
+
+const router = Router();
+
+router.post('/register', [
+    check('email').isEmail().withMessage('Invalid email'),
+    check('password').isLength({min: 6}).withMessage('Password must be at least 6 characters long'),
+    validate
+    ], AuthController.register
+);
+
+router.post('/login', [
+    check('email').isEmail().withMessage('Invalid email'),
+    check('password').notEmpty().withMessage('Password is required'),
+    validate
+  ], AuthController.login);
+
+export default router
---
+++ src/modules/auth/auth.service.ts
@@ -0,0 +1,45 @@
+import { PrismaClient } from '@prisma/client';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+const prisma = new PrismaClient();
+
+
+export const register = async(data: any) => {
+    const hashedPassword = await bcrypt.hash(data.password, 10);
+
+    const user = await prisma.user.create({
+        data: {
+            email: data.email,
+            password: hashedPassword,
+            name: data.name
+        }
+    })
+
+    return user;
+}
+
+export const login = async(data: any) => {
+
+    const user = await prisma.user.findUnique({
+        where: { email: data.email }
+    });
+
+    if(!user){
+        throw new Error('User not found'); 
+    }
+        
+    const isPasswordValid = await bcrypt.compare(data.password, user.password);
+
+    if(!isPasswordValid){
+        throw new Error('Incorrect password'); 
+    }
+
+    const token = jwt.sign(
+        { userId: user.id, email: user.email },
+        process.env.JWT_SECRET as string,
+        { expiresIn: '1h' }
+    )
+
+    return token
+}

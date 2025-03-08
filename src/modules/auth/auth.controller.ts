14a5365624d0d735e3c0ceef47f47baceff405cc
altf4rex <mr.stereo2002@gmail.com>
2025-03-09 00:46:13 +0300

    chore: cookies fix

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -14,10 +14,13 @@ export const login = async(req: Request, res: Response, next: NextFunction) => {
     try{
         const {token, user} = await AuthService.login(req.body);
         res.cookie('token', token, {
-            httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            maxAge: 3600000, // 1 hour
+          httpOnly: true,
+          secure: process.env.NODE_ENV === 'production',
+          sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+          path: '/',
+          maxAge: 7 * 24 * 60 * 60 * 1000,
         })
+        
         res.json({ message: 'Login successful', user });
     } catch(error: any) {
         res.status(401).json({ message: error.message || 'Invalid credentials' });

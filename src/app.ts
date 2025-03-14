45f53a21a444028a64c5299c2ca774eaef098f87
altf4rex <mr.stereo2002@gmail.com>
2025-03-14 19:53:52 +0300

    chore: changed chores settings

--- src/app.ts
+++ src/app.ts
@@ -17,8 +17,8 @@ const app = express();
 app.use(helmet());
 
 app.use(cors({
-    // origin: 'https://task-manager-frontend-seven-zeta.vercel.app',
-    origin: 'http://localhost:3000',
+    origin: 'https://task-manager-frontend-seven-zeta.vercel.app',
+    // origin: 'http://localhost:3000',
     credentials: true, 
   }));
 
